ed86f3e9b0a4908e066fe709f3bffe49b665aee5
Paul Glavich <glav@theglavs.com>
2017-06-22 21:20:45 +1000

    Added config for gmail account

---
+++ Jobs/EmailCollector/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailCollector
+{
+    public class Config
+    {
+        public Config()
+        {
+            ParseConfig();
+
+        }
+
+        private void ParseConfig()
+        {
+            Hostname = ConfigurationManager.AppSettings["Email.hostname"];
+            Username = ConfigurationManager.AppSettings["Email.username"];
+            Password = ConfigurationManager.AppSettings["Email.password"];
+            var portText = ConfigurationManager.AppSettings["Email.port"];
+            var useSslText = ConfigurationManager.AppSettings["Email.useSsl"];
+            if (!string.IsNullOrEmpty(useSslText) && useSslText.ToLowerInvariant() == "true")
+            {
+                UseSSL = true;
+            }
+            int portValue;
+            if (!int.TryParse(portText, out portValue))
+            {
+                Port = 25;
+            }
+        }
+
+        public string Hostname { get; private set; }
+        public int Port { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool UseSSL { get; private set; }
+    }
+}
