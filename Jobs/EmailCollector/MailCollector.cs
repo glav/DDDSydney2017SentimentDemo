5eb2d1650d9039bd19208451edbb28483b56d869
Paul Glavich <glav@theglavs.com>
2017-06-22 21:22:30 +1000

    Used config in email code

--- Jobs/EmailCollector/MailCollector.cs
+++ Jobs/EmailCollector/MailCollector.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenPop.Mime;
+using OpenPop.Pop3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace EmailCollector
 {
-    class MailCollector
+    public class MailCollector
     {
+        public void GetLatestMail(Config config)
+        {
+            using (var client = new Pop3Client())
+            {
+                // Connect to the server
+                client.Connect(config.Hostname, config.Port, config.UseSSL);
+
+                // Authenticate ourselves towards the server
+                client.Authenticate(config.Username, config.Password);
+
+                // Get the number of messages in the inbox
+                int messageCount = client.GetMessageCount();
+
+                // We want to download all messages
+                var allMessages = new List<Message>(messageCount);
+
+                // Messages are numbered in the interval: [1, messageCount]
+                // Ergo: message numbers are 1-based.
+                // Most servers give the latest message the highest number
+                for (int i = messageCount; i > 0; i--)
+                {
+                    allMessages.Add(client.GetMessage(i));
+                }
+
+                // Now do something with the messages
+                //return allMessages;
+            }
+        }
     }
 }
