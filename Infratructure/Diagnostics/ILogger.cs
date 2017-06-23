52ad7c4cd615998853d58a427beb3b3690bc4259
Paul Glavich <glav@theglavs.com>
2017-06-23 17:53:18 +1000

    Added a diagnostic logger

---
+++ Infratructure/Diagnostics/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagnostics
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogError(string message);
+        void LogError(Exception ex, string message);
+    }
+}
