04d082f43d69ad48cf38e3ffad10cb74b0478de5
Paul Glavich <glav@theglavs.com>
2017-06-27 20:56:06 +1000

    Adding scheduler

---
+++ Jobs/JobScheduler/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace JobScheduler
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([TimerTrigger("*/30 0 0 ***")] TimerInfo timer)
+        {
+            Console.WriteLine("Timer fired");
+        }
+    }
+}
