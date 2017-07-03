65db0aa30fe02d7ee4b7f08952436d462d9b1853
Paul Glavich <glav@theglavs.com>
2017-07-03 17:47:46 +1000

    Test ref .Net core from full framework

--- Jobs/EmailAnalyser/Functions.cs
+++ Jobs/EmailAnalyser/Functions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Jobs.Core;
 
 namespace EmailAnalyser
 {
@@ -12,9 +13,12 @@ namespace EmailAnalyser
     {
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        public static void ProcessQueueMessage([QueueTrigger(CloudStorageAssets.AnalyseQueueName)] string message, TextWriter log)
         {
+            log.WriteLine($"received message on{CloudStorageAssets.AnalyseQueueName} queue ");
             log.WriteLine(message);
+            var testclass = new DDD2017DemoAnalyser.Class1();
+            testclass.Test();
         }
     }
 }
