df229d35680b52752e2d4cf8fd5ad8951d6973cc
Paul Glavich <glav@theglavs.com>
2017-07-03 16:52:40 +1000

    Adding constants

--- Jobs/EmailCollector/EmailCollectedEventTrigger.cs
+++ Jobs/EmailCollector/EmailCollectedEventTrigger.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace EmailCollector
 {
-    public class EmailCollectedEvent
+    public class EmailCollectedEventTrigger
     {
         public void Signal()
         {
